c803d166faa16ed36aba3fb14add0daab23bcb6e
Momen-Elshamy <eng.momen.adel@gmail.com>
2023-06-11 16:06:19 +0300

    fix display data and index html

--- js/script.js
+++ js/script.js
@@ -2,49 +2,47 @@ let rowData = document.getElementById("rowData");
 let datacontainer = document.getElementById("cont");
 var data = [];
 
-getdata();
-function getdata() {
-  fetch(
-    `https://premast.com/wp-json/pmst/v1/products?orderby=date&order=DESC&category_slug=presentation&status=any`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      rowData.innerHTML = data.data;
-      displaydata();
-    })
-    .catch((err) => console.log(err));
+window.onload = async function () {
+   const data = await getData();
+   if (!data) return console.log("no data");
+
+   displaydata(data.data);
+};
+
+function displaydata(data) {
+   console.log("data : ", data);
+
+   const templatesContainer = document.getElementById("templatesContainer");
+   data.map((item) => {
+      const template = document.createElement("div");
+      template.innerHTML = `
+      <div>
+      ${item.title}
+      </div>
+      `;
+      templatesContainer.appendChild(template);
+   });
 }
 
-function displaydata() {
-  var cols = ``;
-  for (i = 0; i < data.length; i++) {
-    cols += `
-    <div class = "col-md-3">
-    <div class = "container" >
-    <img class="w-100" src="${data[i].image}" alt="" />
-    </div>
-    </div>
-
- 
-
-    `;
-  }
-
-  // console.log("cols : " + JSON.stringify({ cols }));
-  console.log("cols : ", { cols });
-
-  document.getElementById("rowData").innerHTML = cols;
+async function getData() {
+   const data = await fetch(
+      `https://premast.com/wp-json/pmst/v1/products?orderby=date&order=DESC&category_slug=presentation&status=any`
+   ).then((res) => {
+      console.log("data : ", res);
+      return res.json();
+   });
+   console.log("data : ", data);
+   return data;
 }
 
 getdatacontainer();
 
-function getdatacontainer() {
-  fetch(`https://premast.com/wp-json/pmst/v1/category?id=884`)
-    .then((res) => res.json())
-    .then((container) => {
-      console.log(container);
-      datacontainer.innerHTML = container.heading;
-    })
-    .catch((err) => console.log(err));
+async function getdatacontainer() {
+   fetch(`https://premast.com/wp-json/pmst/v1/category?id=884`)
+      .then((res) => res.json())
+      .then((container) => {
+         console.log(container);
+         datacontainer.innerHTML = container.heading;
+      })
+      .catch((err) => console.log(err));
 }
